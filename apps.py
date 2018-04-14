36a143e3788cf022f49b13822c9037a396faf568
Janer B. Melo <janerbastos@gmail.com>
2018-04-14 00:13:26 -0300

    Nomeação da class do modulo apps

--- apps.py
+++ apps.py
@@ -4,5 +4,8 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class SheradConfig(AppConfig):
+class DatabasesConfig(AppConfig):
     name = 'databases'
+
+    def ready(self):
+        import databases.signals.database
